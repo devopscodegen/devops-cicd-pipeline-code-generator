b7e5724576f228d0515bbac377669bfc10c39bcb
Sagar Velankar <Sagar Velankar>
2024-08-30 21:18:34 +0530

    add nextra documentation website

---
+++ docs/next.config.mjs
@@ -0,0 +1,27 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  latex: true,
+  flexsearch: {
+    codeblocks: false
+  },
+  defaultShowCopyCode: true
+});
+
+export default withNextra({
+  basePath: '/devops-cicd-pipeline-code-generator',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  redirects: () => [
+    {
+      source: '/',
+      destination: '/docs',
+      permanent: true
+    }
+  ]
+})
